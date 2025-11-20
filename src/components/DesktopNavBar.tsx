e4659f073e159ffbed6374ae19fe61a0c1181ad7
Rahul-Encoded <rahulm03@outlook.com>
2025-11-20 12:49:31 +0530

    Refactor DesktopNavbar to support user authentication and profile linking

--- src/components/DesktopNavBar.tsx
+++ src/components/DesktopNavBar.tsx
@@ -1,8 +1,37 @@
+import { currentUser } from "@clerk/nextjs/server";
 import ThemeToggle from "./ThemeToggle";
+import { SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { UserIcon } from "lucide-react";
 
-export default function DesktopNavbar(){
+export default async function DesktopNavbar(){
+
+	const user = await currentUser();
 
 	return(
+		<div className="hidden md:flex items-center space-x-4">
 		<ThemeToggle></ThemeToggle>
-	)
+
+		 {user ? (
+			<>
+				<Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link
+              href={`/profile/${
+                user.username ?? user.emailAddresses[0].emailAddress.split("@")[0]
+              }`}
+            >
+              <UserIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Profile</span>
+            </Link>
+          </Button>
+			</>
+		):(
+			<SignInButton mode="modal">
+				<Button variant="default">Sign-In</Button>
+			</SignInButton>
+		)
+			}
+		</div>
+	);
 }
