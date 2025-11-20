0a8005baaf3741ad4a6c99a5a661a3be6d8b1ac1
Rahul-Encoded <rahulm03@outlook.com>
2025-11-20 13:14:18 +0530

    Refactor DesktopNavbar to remove user authentication logic and simplify structure

--- src/components/DesktopNavBar.tsx
+++ src/components/DesktopNavBar.tsx
@@ -1,37 +1,13 @@
-import { currentUser } from "@clerk/nextjs/server";
+import SignIn from "./SignIn";
 import ThemeToggle from "./ThemeToggle";
-import { SignInButton } from "@clerk/nextjs";
-import { Button } from "./ui/button";
-import Link from "next/link";
-import { UserIcon } from "lucide-react";
 
-export default async function DesktopNavbar(){
 
-	const user = await currentUser();
+export default function DesktopNavbar(){
 
 	return(
-		<div className="hidden md:flex items-center space-x-4">
+		<div className="flex items-center space-x-4">
 		<ThemeToggle></ThemeToggle>
-
-		 {user ? (
-			<>
-				<Button variant="ghost" className="flex items-center gap-2" asChild>
-            <Link
-              href={`/profile/${
-                user.username ?? user.emailAddresses[0].emailAddress.split("@")[0]
-              }`}
-            >
-              <UserIcon className="w-4 h-4" />
-              <span className="hidden lg:inline">Profile</span>
-            </Link>
-          </Button>
-			</>
-		):(
-			<SignInButton mode="modal">
-				<Button variant="default">Sign-In</Button>
-			</SignInButton>
-		)
-			}
+		<SignIn></SignIn>
 		</div>
 	);
 }
