0afb78f85b749a07fd11da786917e6dc5853f961
Rahul-Encoded <rahulm03@outlook.com>
2025-11-21 11:39:02 +0530

    landing Pange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import DashBoard from "@/components/DashBoard";
+
 export default async function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
+      <DashBoard></DashBoard>
     </div>
   );
 }
