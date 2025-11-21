4dbea40675a851d3d9c380d70a1e173c90436af4
Rahul-Encoded <rahulm03@outlook.com>
2025-11-21 12:50:35 +0530

    feat: add new `LinkButton` component for styled navigation and Clerk sign-in.

--- src/components/rahui/link-button.tsx
+++ src/components/rahui/link-button.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 interface LinkButtonProps {
@@ -7,8 +8,10 @@ interface LinkButtonProps {
 
 export default function LinkButton({ content, link }: LinkButtonProps) {
     return (
-        <Link href={link} className="inline-block px-12 py-4 text-lg font-bold text-secondary bg-linear-to-r from-green-500 to-green-900 rounded-xl shadow-2xl shadow-green-600/40 hover:bg-green-400 transition transform hover:scale-105 duration-500">
-            {content}
-        </Link>
+        <SignInButton mode="modal">
+            <Link href={link} className="inline-block px-12 py-4 text-lg font-bold text-secondary bg-linear-to-r from-green-500 to-green-900 rounded-xl shadow-2xl shadow-green-600/40 hover:bg-green-400 transition transform hover:scale-105 duration-500">
+                {content}
+            </Link>
+        </SignInButton>
     )
 }
