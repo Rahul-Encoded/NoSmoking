e41d30ae9278b36b842d257286546303ba31eef4
Rahul-Encoded <rahulm03@outlook.com>
2025-11-21 12:53:18 +0530

    feat: add home page to display dashboard for unauthenticated users.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import DashBoard from "@/components/DashBoard";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
 
+  const user = await currentUser();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
-      <DashBoard></DashBoard>
+
+      {user ? (<></>) : (<DashBoard></DashBoard>)}
+
     </div>
   );
 }
