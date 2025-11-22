9b274450d8f3c18395ca86708b1f5c964f2e3728
Rahul-Encoded <rahulm03@outlook.com>
2025-11-22 11:11:25 +0530

    update user data function

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -37,4 +37,59 @@ export async function syncUser() {
 		console.log("Error in syncUser", error);
 		throw error;
 	}
-} 
+}
+
+export async function getUserByClerkId(clerkId: string) {
+	// console.log("Fetching user by Clerk ID:", clerkId);
+
+	const user = await prisma.user.findUnique({
+		where: { clerkId },
+	});
+
+	if (!user) {
+		console.error("No user found for Clerk ID:", clerkId);
+	}
+
+	return user;
+}
+
+
+export async function dbUser() {
+
+	const { userId: clerkId } = await auth();
+
+	if (!clerkId) return null;
+
+	const user = await getUserByClerkId(clerkId);
+
+	if (!user) throw new Error("User Not found");
+
+	return user.id;
+
+
+}
+
+export async function updateUserData(date: Date, cost: number, dailyAvg: number, duration: number) {
+	try {
+		const userId = await dbUser();
+
+		if (!userId) return;
+
+		const update = await prisma.user.update({
+			where: {
+				id: userId,
+			},
+			data: {
+				dob: date,
+				costPerCigg: cost,
+				initDailyAvg: dailyAvg,
+				duration: duration,
+			}
+		});
+
+		return { success: true, update };
+	} catch (error) {
+		console.error("Error in updateUserData", error);
+		return { success: false, error }
+	}
+}
