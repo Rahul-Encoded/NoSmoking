f7291ecd9db324500a3a496c371bc9a14f62edba
Rahul-Encoded <rahulm03@outlook.com>
2025-11-22 23:54:14 +0530

    Schema Update

---
+++ prisma/migrations/20251122175353_schema_update/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "dietQuality" TEXT,
+ADD COLUMN     "jobHours" DOUBLE PRECISION,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "physicalActivity" TEXT,
+ADD COLUMN     "sleepQuality" TEXT;
