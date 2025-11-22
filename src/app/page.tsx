60fe545ad9744afb27dd54a5571299d22278c7ab
Rahul-Encoded <rahulm03@outlook.com>
2025-11-22 11:19:02 +0530

    Revert "Merge branch 'main' of https://github.com/Rahul-Encoded/NoSmoking"
    This reverts commit 4f0893fdeb6f7554baca5512a24b533259023d66, reversing
    changes made to 83677f85b047555bd5819a2ea83328c451816b3e.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import DashBoard from "@/components/DashBoard";
-import Form from "@/components/Form";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
@@ -9,7 +8,7 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
 
-      {user ? (<Form></Form>) : (<DashBoard></DashBoard>)}
+      {user ? (<></>) : (<DashBoard></DashBoard>)}
 
     </div>
   );
